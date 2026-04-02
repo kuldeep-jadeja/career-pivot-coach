a0a26185204fc16bd63bf1f62693acfb8cc12428
kuldeep-jadeja <kuldeepjadeja7414@gmail.com>
2026-04-02 13:36:36 +0530

    feat(02-03): support anonymous flow integration test runtime
    - add ResizeObserver polyfill in test setup for Radix/cmdk components
    - enable anonymous flow integration test to run green in jsdom

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom/vitest';
+
+class MockResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+if (typeof global.ResizeObserver === 'undefined') {
+  // @ts-expect-error test polyfill
+  global.ResizeObserver = MockResizeObserver;
+}
